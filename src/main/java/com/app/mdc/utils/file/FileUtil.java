3fa49cd3dddb50565b3380b5d3313238de224102
wrf <1426878506@qq.com>
2020-02-05 10:36:58 +0800

    修改

--- src/main/java/com/app/mdc/utils/file/FileUtil.java
+++ src/main/java/com/app/mdc/utils/file/FileUtil.java
@@ -1,9 +1,6 @@
 package com.app.mdc.utils.file;
 
-import org.jodconverter.OfficeDocumentConverter;
-import org.jodconverter.office.DefaultOfficeManagerBuilder;
-import org.jodconverter.office.OfficeException;
-import org.jodconverter.office.OfficeManager;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Async;
@@ -19,7 +16,6 @@ public class FileUtil {
 
 	private static Logger logger = LoggerFactory.getLogger(FileUtil.class);
 
-	private static OfficeManager officeManager=null;
 	/**
 	 * 将MultipartFile保存到指定的路径下
 	 * 
@@ -99,98 +95,6 @@ public class FileUtil {
 		return file.exists();
 	}
 
-	/**
-	 * 开启OfficeManager服务
-	 * @return OfficeManager
-	 */
-	public static OfficeManager processStart(String libreOfficePath){
-		//String libreOfficePath = picConfig.getLibreOffice();
-		logger.info("准备开启officeManager服务!");
-		try {
-			// 此类在jodconverter-core中3版本中存在，在2.2.2版本中不存在
-			DefaultOfficeManagerBuilder configuration = new DefaultOfficeManagerBuilder();
-			// libreOffice的安装目录
-			logger.info("设置libreOffice安装目录:"+libreOfficePath);
-			configuration.setOfficeHome(new File(libreOfficePath));
-			// 设置端口号
-			logger.info("设置服务端口:"+8100);
-			configuration.setPortNumbers(8100);
-			// 设置任务执行超时为5分钟
-			logger.info("设置任务执行超时时间:5分钟");
-			configuration.setTaskExecutionTimeout(1000 * 60 * 5L);
-			// 设置任务队列超时为24小时
-			logger.info("设置任务队列超时时间:24小时");
-			configuration.setTaskQueueTimeout(1000 * 60 * 60 * 24L);
-			// 开启转换服务
-			logger.info("开启转换服务");
-			officeManager = configuration.build();
-			officeManager.start();
-			logger.info("服务开启成功!");
-		}catch (OfficeException e){
-			logger.error("officeManager启动异常",e);
-		}
-		return officeManager;
-	}
-
-	/**
-	 * 开始文件转换
-	 * @param inputFile 需要转换的文件
-	 * @param fileChangePath 转换后文件保存地址
-	 * @param libreOffice libreOffice程序目录
-	 * @return 转换后文件地址
-	 */
-	public static String libreOfficeAndJodconverter(String inputFile,String fileChangePath,String libreOffice){
-		logger.info("开始将文件"+inputFile+"转换为pdf文件");
-		String outputFile = "";
-		try {
-			if(officeManager == null || !officeManager.isRunning()){
-				officeManager = FileUtil.processStart(libreOffice);
-			}
-			logger.info("获取需要转换的文件:"+inputFile);
-			File src = new File(inputFile);
-			if(src.exists()){
-				File changeFile = new File(fileChangePath);
-				if(!changeFile.exists()){
-					changeFile.mkdir();
-				}
-				outputFile = fileChangePath+UUID.randomUUID().toString().replace("-","")+".pdf";
-				logger.info("设置转换后的文件:"+outputFile);
-				File dst = new File(outputFile);
-				OfficeDocumentConverter converter = new OfficeDocumentConverter(officeManager);
-				logger.info("转换文件"+inputFile+"为"+outputFile);
-				converter.convert(src,dst);
-			}else{
-				logger.error("需要转换的文件不存在，请重新选择需要转换的文件!");
-			}
-			return outputFile;
-		} catch (OfficeException e){
-			logger.error("文件转换失败",e);
-			return outputFile;
-		}
-		finally {
-//			FileUtil fileUtil = new FileUtil();
-//			fileUtil.processStop(outputFile,officeManager);
-		}
-		//return outputFile;
-	}
-
-
-	/**
-	 * 停止office服务
-	 * @param path 文件路径
-	 * @param officeManager officeManager
-	 */
-	@Async
-	public void processStop(String path,OfficeManager officeManager){
-		if (officeManager != null && !"".equals(path)){
-			try {
-				logger.info("关闭officeManager服务!");
-				officeManager.stop();
-			}catch (OfficeException oc){
-				logger.error("服务关闭失败",oc);
-			}
-		}
-	}
 
 	public static String xDocServiceChangeFile(String inputFile,String fileChangePath){
 		XDocService xDocService = new XDocService();
