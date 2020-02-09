6fc75b213b20439ef90a6e5b5bac703f5372dd44
endlessBUG <1424547204@qq.com>
2020-02-09 09:39:19 +0800

    获取升级信息

--- src/test/java/com/app/mdc/usercontract/UserContractTest.java
+++ src/test/java/com/app/mdc/usercontract/UserContractTest.java
@@ -27,6 +27,7 @@ public class UserContractTest {
     public void getUpgradePriceDifference(){
         Map<String,String> param  = new HashMap<>();
         param.put("ucId","47");
+        param.put("upgradeId","4");
         String s = HttpUtil.doPost(HOST + "/userContract/getUpgradePriceDifference", param,"0121188e-f37b-4789-b7ae-95fb93633862");
         System.out.println(s);
     }
@@ -36,6 +37,7 @@ public class UserContractTest {
         Map<String,String> param  = new HashMap<>();
         param.put("userId","155");
         param.put("ucId","47");
+        param.put("upgradeId","4");
         param.put("payToken","123");
         String s = HttpUtil.doPost(HOST + "/userContract/upgrade", param,"0121188e-f37b-4789-b7ae-95fb93633862");
         System.out.println(s);
